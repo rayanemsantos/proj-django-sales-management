cd9460c62c353eaf778b8f9882b69d1d10ce313e
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2022-11-23 23:46:51 -0300

    Cria classes venda e produtos da venda

---
+++ sale/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.utils import timezone
+from product.models import Product
+from customer.models import Customer
+from seller.models import Seller
+
+
+class Sale(models.Model):
+    ''' Classe que representa uma venda '''
+    access_key = models.CharField(
+        "Nota fiscal", max_length=44, null=True, blank=True)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    seller = models.ForeignKey(
+        Seller, on_delete=models.SET_NULL, blank=True, null=True)
+    register_datetime = models.DateTimeField(
+        "Data de registro", editable=False)
+
+    def __str__(self):
+        return '# {}'.format(str(self.pk))
+
+    def save(self, *args, **kwargs):
+        if not self.register_datetime:
+            self.register_datetime = timezone.now()
+        return super(Sale, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'venda'
+        verbose_name_plural = 'vendas'
+
+
+class SaleProduct(models.Model):
+    ''' Classe que representa a agenda de comissão de um produto '''
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False)
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE, null=False)
+    quantity = models.PositiveIntegerField(
+        "Quantidade", blank=False, null=False)
+    commission_applied = models.DecimalField(
+        "Comissão aplicada", max_digits=3, decimal_places=1, default=0)
+
+    def __str__(self):
+        return "{} x {}".format(self.product.description, self.quantity)
+
+    def save(self, *args, **kwargs):
+        return super(SaleProduct, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'produto da venda'
+        verbose_name_plural = 'produtos da venda'
